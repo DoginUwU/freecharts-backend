e4b3428d25fe954a12ac37b9ba86d2477e2a9283
Luiz E <doginuwu@gmail.com>
2026-03-08 18:42:48 -0300

    fix: router path

--- src/routes/geo.route.ts
+++ src/routes/geo.route.ts
@@ -4,6 +4,6 @@ import { GeoController } from "../controllers/geo.controller";
 const geoRoutes = Router();
 const geoController = new GeoController();
 
-geoRoutes.get("/airport/:icao", geoController.loadAirportOSM);
+geoRoutes.get("/airports/:icao", geoController.loadAirportOSM);
 
 export { geoRoutes };
